218cb9c7d06b9e85526b1881a7e4cb940b376bf6
xupt-hx666 <2295734689@qq.com>
2025-03-20 10:50:57 +0800

    Update args.py

--- edu/args.py
+++ edu/args.py
@@ -12,7 +12,7 @@ def args_parser():
     parser.add_argument('--input_dim', type=int, default=20, help='输入特征维度')
     parser.add_argument('--num_classes', type=int, default=5, help='分类类别数')
     parser.add_argument('--lr', type=float, default=0.01, help='学习率')
-    parser.add_argument('--C', type=float, default=0.5, help='每轮参与客户端比例')
+    parser.add_argument('--C', type=float, default=1.0, help='每轮参与客户端比例')
     parser.add_argument('--B', type=int, default=32, help='本地批量大小')
     parser.add_argument('--optimizer', type=str, default='adam', help='优化器')
     parser.add_argument('--device', default=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
